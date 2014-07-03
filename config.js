9bc3d71db909d0b9420e50d8cef559f0800e3846
Christopher Rolfe <chris@mnadigital.com>
2014-07-03 13:07:43 +0100

    Updating the github pages

---
+++ config.js
@@ -0,0 +1,12 @@
+// Add items to the toolbar here
+var RichConfig = {
+	toolbar: "smiley,left,middle,right,full,indent,outdent,ol,ul,a,i,b,u,s,sup,sub,hr,font,fontSize,color,background,all,unformat,image,test,html,wrap"
+}
+
+var ToolbarItems = {
+	"smiley": {
+		"command": "insertImage",
+		"value": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSpNVtBVykCdAeYWuZA-TvZe0jcBD4NTk1_uMh_ZagWqbgSozmXWm7yKA"
+	},
+}
+
