f5158c8e1eff6f5f647af1b0a9b6e96ced14244b
Christopher Rolfe <chris@mnadigital.com>
2014-09-22 16:58:17 +0100

    Starting to define the configuration file

--- example/RichConfig.js
+++ example/RichConfig.js
@@ -1,12 +1,5 @@
-// Add items to the toolbar here
-var RichConfig = {
-	toolbar: "b,i,u,smiley,ul,link,background"
-}
-
-// Create custom toolbar items
-var ToolbarItems = {
-	"smiley": {
-		"command": "insertImage",
-		"value": "http://bit.ly/1shQN2U"
+var Rich.config = {
+	toolbars: {
+		default: ["bold"]
 	}
 }
